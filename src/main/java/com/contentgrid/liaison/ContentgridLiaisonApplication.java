46a3d59b5a380df897499aacb59ed8798bf93bfd
Roel <roel.schevenels@xenit.eu>
2023-09-18 13:57:26 +0200

    Add test

--- src/main/java/com/contentgrid/liaison/ContentgridLiaisonApplication.java
+++ src/main/java/com/contentgrid/liaison/ContentgridLiaisonApplication.java
@@ -2,6 +2,7 @@ package com.contentgrid.liaison;
 
 import com.contentgrid.liaison.config.LiaisonProperties;
 import com.contentgrid.liaison.kubernetes.KubernetesDiscovery;
+import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClientBuilder;
 import io.fabric8.kubernetes.client.impl.KubernetesClientImpl;
 import org.springframework.boot.ApplicationRunner;
@@ -19,8 +20,13 @@ public class ContentgridLiaisonApplication {
     }
 
     @Bean
-    KubernetesDiscovery discovery(LiaisonProperties properties) {
-        return new KubernetesDiscovery(properties.getDiscovery(), new KubernetesClientBuilder().build());
+    KubernetesDiscovery discovery(LiaisonProperties properties, KubernetesClient client) {
+        return new KubernetesDiscovery(properties.getDiscovery(), client);
+    }
+
+    @Bean
+    KubernetesClient k8sClient() {
+        return new KubernetesClientBuilder().build();
     }
 
     @Bean
